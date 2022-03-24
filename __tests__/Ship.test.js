46d08d65743531bbbd05d4cd8b8e7a5b3ef301cc
Thomas <thomas.bogle@outlook.com>
2022-03-24 13:53:35 +0000

    Refactored ship test suite

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -3,67 +3,57 @@ const Port = require('../src/port');
 const Itinerary = require('../src/itinerary');
 
 describe('Ship', () => {
-    it('can be instantiated', () => {
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
-        const ssTidal = new Ship(itinerary);
+    describe('with ports and an itinerary', () => {
+        let ssTidal;
+        let dover;
+        let calais;
+        let itinerary; 
 
-        expect(ssTidal).toBeInstanceOf(Object);
-    });
-    it('ship gets added to port on instantiation', () => {
-        const dover = new Port('Dover');
-        const itinerary = new Itinerary([dover]);
-        const ssTidal = new Ship(itinerary);
-
-        expect(dover.ships).toContain(ssTidal);
-    });
-    it('has a starting port', () => {     
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
-        const ssTidal = new Ship(itinerary);
-
-        expect(ssTidal.currentPort).toBe(port);
-    });
-
+        beforeEach(() => {
+            dover = new Port('Dover');
+            calais = new Port('Calais');
+            itinerary = new Itinerary([dover, calais]);
+            ssTidal = new Ship(itinerary);
+        });
+        it('can be instantiated', () => {
     
-    it('ship can set sail', () => {
-        const dover = new Port('Dover');
-        const calais = new Port('Calais');
-        const itinerary = new Itinerary([dover, calais]);
-        const ssTidal = new Ship(itinerary);
-
-        ssTidal.setSail();
-
-        expect(ssTidal.currentPort).toBeFalsy();
-        expect(ssTidal.previousPort).toBe(dover);
-        expect(dover.ships[0]).not.toContain(ssTidal);
-        // i.e. not at the startingPort anymore
-    });
-
-
-    it('can dock at a different port', () => {
-        const dover = new Port('Dover');
-        const calais = new Port('Calais');
-        const itinerary = new Itinerary([dover, calais]);
-        const ssTidal = new Ship(itinerary);
-
-        ssTidal.setSail();
-        ssTidal.dock(); 
-        // no port arguement as ship docks at the next port on an itinerary.
-
-        expect(ssTidal.currentPort).toBe(calais);
-        expect(ssTidal.previousPort).toBe(dover);
-        expect(calais.ships).toContain(ssTidal);
-    });
-    it('can\'t sail further than its itinerary', () => {
-        const dover = new Port('Dover');
-        const calais = new Port('Calais');
-        const itinerary = new Itinerary([dover, calais]);
-        const ssTidal = new Ship(itinerary);
+            expect(ssTidal).toBeInstanceOf(Object);
+        });
+        it('has a starting port', () => {
+    
+            expect(ssTidal.currentPort).toBe(dover);
+        });
+        it('ship can set sail', () => {
+    
+            ssTidal.setSail();
+            
+            expect(ssTidal.currentPort).toBeFalsy();
+            // i.e. not at the startingPort anymore
+            expect(ssTidal.previousPort).toBe(dover);
+            expect(dover.ships[0]).not.toContain(ssTidal);
+        });
+        it('ship gets added to port on instantiation', () => {
+    
+            expect(dover.ships).toContain(ssTidal);
+        });
+        it('can dock at a different port', () => {
+            
+            ssTidal.setSail();
+            ssTidal.dock(); 
+            // no port arguement as ship docks at the next port on an itinerary.
+    
+            expect(ssTidal.currentPort).toBe(calais);
+            expect(ssTidal.previousPort).toBe(dover);
+            expect(calais.ships).toContain(ssTidal);
+        });
+        it('can\'t sail further than its itinerary', () => {
+            
+            ssTidal.setSail();
+            ssTidal.dock();
+    
+            expect(() => ssTidal.setSail()).toThrowError('End of itinerary reached');
+        });
         
-        ssTidal.setSail();
-        ssTidal.dock();
+    });  
 
-        expect(() => ssTidal.setSail()).toThrowError('End of itinerary reached');
-    });
 });
