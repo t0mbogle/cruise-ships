0d917ea4491466c69551e2e93249fab2ff049231
Thomas <thomas.bogle@outlook.com>
2022-03-29 20:40:53 +0100

    Added itinerary/ports to GUI

--- src/controller.js
+++ src/controller.js
@@ -14,7 +14,29 @@ Controller.prototype = {
         document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
         backgroundIndex += 1;
     }, 1000);
-  }
+    },
+
+    renderPorts(ports) {
+      const portsElement = document.querySelector('#ports')
+      portsElement.style.width = '0px';
+
+      ports.forEach((port, index) => {
+        const newPortElement = document.createElement('div');
+        newPortElement.className = 'port';
+
+        newPortElement.dataset.portName = port.name;
+        newPortElement.dataset.portIndex = index;
+        /* every new port element gets added to the class #ports and is given 
+        data details, such as index and name. */
+
+        portsElement.appendChild(newPortElement);
+
+        const portsElementWidth = parseInt(portsElement.style.width, 10);
+        portsElement.style.width = `${portsElementWidth + 256}px`;
+        /* how much the screen extends per new port element. It is able 
+        to do this because of #viewport overview-x: auto; in the CSS file :) */
+      });
+    }
 }
 
   if(typeof module !== 'undefined' && module.exports) {
