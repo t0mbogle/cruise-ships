5ce3a5ee730c98df021a4cc47260672240dffe6d
Thomas <thomas.bogle@outlook.com>
2022-03-29 14:00:22 +0100

    Added moving sea to GUI

---
+++ src/controller.js
@@ -0,0 +1,17 @@
+function Controller() {
+    this.initialiseSea();
+}
+
+Controller.prototype = {
+    initialiseSea() {
+    const backgrounds = [
+        'images/water0.png',
+        'images/water1.png',
+    ];
+    let backgroundIndex = 0;
+    window.setInterval(() => {
+        document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
+        backgroundIndex += 1;
+    }, 1000);
+  }
+}
